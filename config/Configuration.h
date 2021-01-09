e464613bf6abe6122faf27e3a478fd6df1440270
Martin R <hades32@gmail.com>
2021-01-09 01:36:12 +0100

    fix bed size

--- config/Configuration.h
+++ config/Configuration.h
@@ -1124,8 +1124,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 255
-#define Y_BED_SIZE 255
+#define X_BED_SIZE 250
+#define Y_BED_SIZE 250
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS -5
