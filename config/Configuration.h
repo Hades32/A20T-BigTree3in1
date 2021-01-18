573fd776ff3aa0497a70f2d268749572077a3008
Martin R <hades32@gmail.com>
2021-01-18 06:44:47 +0100

    fix extruder steps/mm

--- config/Configuration.h
+++ config/Configuration.h
@@ -743,7 +743,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.3, 80.8, 400, 340 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.3, 80.8, 400, 409 }
 
 /**
  * Default Max Feed Rate (mm/s)
